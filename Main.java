dca27c430dcb0cb9d5a4e927fb31fde681c164bc
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-11-21 14:43:17 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void Main(String[] args){
+        Veiculo c1 = new Veiculo(); //criei o veiculo
+        c1.setAno(2010);
+        c1.setModelo("Corolla");
+        c1.setMarcaVeiculo("CMW");
+        c1.imprimirVeiculo();
+    }
+}

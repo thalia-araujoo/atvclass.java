b77067bed2d5892743809a0c2b1aca01fcf9bdf7
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-11-22 15:36:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
-    public static void Main(String[] args){
-        Veiculo c1 = new Veiculo(); //criei o veiculo
+    public static void main(String[] args){
+        Veiculos c1 = new Veiculos(); //criei o veiculo
         c1.setAno(2010);
         c1.setModelo("Corolla");
         c1.setMarcaVeiculo("CMW");

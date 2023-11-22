5c30cda457d69efd783be70fbf0b33ee6b064f10
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-11-22 15:46:23 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -2,7 +2,14 @@ public class Veiculos{
     private String MarcaVeiculo;
     private int Ano;
     private String Modelo;
-    
+    private int quilometragem;
+
+    public void setquilometragem(int quilometragem){
+        this.quilometragem=quilometragem;
+    }
+    public int getquilometragem(){
+        return this.quilometragem;
+    }
 
     public void setMarcaVeiculo(String MarcaVeiculo){
         this.MarcaVeiculo=MarcaVeiculo;
@@ -32,6 +39,7 @@ public class Veiculos{
         System.out.println("Ano: "+this.Ano);
         System.out.println("Modelo: "+this.Modelo);
         System.out.println("Marca Do Veiculo: "+this.MarcaVeiculo);
+        System.out.println("quilometragem Do Veiculo: "+this.quilometragem);
     }
 
 }
